0381e2238da9bc0177e607b516f83c964354003e
Laura <l.maya10@uniandes.edu.co>
2019-11-02 14:56:12 -0500

    Build

---
+++ front/build/precache-manifest.87d7a834eb271ff3d2dae800f913d30a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b23d94e8f4947d11bc0145ce32747e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd2b9ece0add5dcc194c",
+    "url": "/static/css/main.60381c7a.chunk.css"
+  },
+  {
+    "revision": "14a4da5d3b229d3d016c",
+    "url": "/static/js/2.cd4c04be.chunk.js"
+  },
+  {
+    "revision": "cd2b9ece0add5dcc194c",
+    "url": "/static/js/main.997bdc55.chunk.js"
+  },
+  {
+    "revision": "ebd3a683af0fa2dbb56f",
+    "url": "/static/js/runtime-main.7139b951.js"
+  }
+]);
