3a5a232425fd68db55ded4b235d911dd25a11434
Laura <l.maya10@uniandes.edu.co>
2019-11-03 10:39:43 -0500

    Un poco visualizacio paseos

--- front/src/Components/Grupos.js
+++ front/src/Components/Grupos.js
@@ -3,11 +3,15 @@ import Paseo from './Paseo.js';
 
 const Grupos =(props)=>{
 
-  const renderPaseos = () => props.paseos.map(d => <Paseo key={d.nombre} nombre ={d.nombre} ruta = {d.ruta} fecha = {d.fecha} puntoEncuentro = {d.puntoEncuentro} numInscritas = {d.numInscritas} />)
+  const renderPaseos = () => props.paseos.map(d => 
+  	<div>
+			<Paseo key={d.nombre} nombre ={d.nombre} ruta = {d.ruta} fecha = {d.fecha} puntoEncuentro = {d.puntoEncuentro} numInscritas = {d.numInscritas} />
+			<br/>
+  	</div>)
 	
   return(
       <div>
-        <h1>Se esta mostrando Grupos</h1>
+        <h1>Rutas Actuales</h1>
         <ul class="ListaPaseos">
         	{renderPaseos()}
         </ul>
--- front/src/Components/Paseo.js
+++ front/src/Components/Paseo.js
@@ -1,16 +1,36 @@
 import React from 'react';
-
+import './Paseo.css';
 
 const Paseo =(props)=>{
 	
   return(
-      <div>
-        <h2> {props.nombre} </h2>
-        <p> Ruta: {props.ruta} </p> 
-        <p>{props.numInscritas} </p>
-        <p>{props.puntoEncuentro} </p>
-        <p>{props.fecha} </p>
-        <br/>
+      <div className = "container" id= "TablaPaseo">
+      	<div className = "row">
+      		<div className = "col-md-12">
+						<p> {props.nombre} </p>
+					</div>
+				</div>
+				<div className = "row">
+					<div className = "col-md-8">
+						<p> Ruta: {props.ruta} </p>
+					</div>
+					<div className = "col-md-4">
+						<p> Participantes: {props.numInscritas} </p>
+					</div>  
+				</div>
+				<div className = "row">
+					<div className = "col-md-12">
+						<p> Punto de Encuentro: {props.puntoEncuentro} </p>
+					</div>
+				</div>
+				<div className = "row">
+					<div className = "col-md-8">
+						<p> Fecha: {props.fecha} </p>
+					</div>
+					<div className = "col-md-4">
+						<button id="BotonInscribirse"> Inscribirse </button>
+					</div>
+				</div>
       </div>
     )
 }
