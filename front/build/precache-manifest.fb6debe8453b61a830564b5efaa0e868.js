db926a2078f798298502cf62b7b889522afce331
Francisco Mcallister Ortiz <fj.mac@uniandes.edu.co>
2019-11-02 20:32:05 -0500

    merge

---
+++ front/build/precache-manifest.fb6debe8453b61a830564b5efaa0e868.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e437ab8c05f5681751b4bf824d35a89a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8097897a349fb7e152f4",
+    "url": "/static/css/main.60381c7a.chunk.css"
+  },
+  {
+    "revision": "6cbac3e0e339561dc3b5",
+    "url": "/static/js/2.6f84791d.chunk.js"
+  },
+  {
+    "revision": "8097897a349fb7e152f4",
+    "url": "/static/js/main.5826588b.chunk.js"
+  },
+  {
+    "revision": "ebd3a683af0fa2dbb56f",
+    "url": "/static/js/runtime-main.7139b951.js"
+  }
+]);
