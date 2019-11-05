d19dd1e5914eb9c6ccd9283cb1174e7de3744b62
Francisco Mcallister Ortiz <fj.mac@uniandes.edu.co>
2019-11-04 19:50:34 -0500

    camb

--- front/src/Components/NavBar.js
+++ front/src/Components/NavBar.js
@@ -13,25 +13,14 @@ function salir(){
       <div className = "NavBar">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="navbar-brand"><Link to="/">Tu Mejor Ruta</Link></div>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        </button>
         <div className="navbar-brand"><Link to="/grupos">Grupos</Link></div>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        </button>
         <div className="navbar-brand"><Link to="/crear">Nuevo Grupo</Link></div>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        </button>
-
         {props.usuario!=="No hay usuario" ?
         <div>
             <div className="navbar-brand"><Link onClick={()=>salir()}>LogOut</Link></div>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            </button>
         </div>:
         <div>
             <div className="navbar-brand"><Link to="/login">LogIn</Link></div>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            </button>
         </div>}
     </nav>
       </div>
